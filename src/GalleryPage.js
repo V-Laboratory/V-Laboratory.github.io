772d227e98f8ae5a22075be1b8f20a2233601d65
Ubin108 <ybin0108@gmail.com>
2025-03-14 14:24:02 +0900

    edit image

--- src/GalleryPage.js
+++ src/GalleryPage.js
@@ -8,12 +8,12 @@ const albums = [
         description: '2025.02.26.',
         images: [
             { src: process.env.PUBLIC_URL + '/gallery/2025_aaai_1.jpg', alt: '2025_aaai_1' },
+            { src: process.env.PUBLIC_URL + '/gallery/2025_aaai_7.jpg', alt: '2025_aaai_7' },
             { src: process.env.PUBLIC_URL + '/gallery/2025_aaai_2.jpg', alt: '2025_aaai_2' },
             { src: process.env.PUBLIC_URL + '/gallery/2025_aaai_6.jpg', alt: '2025_aaai_6' },
             { src: process.env.PUBLIC_URL + '/gallery/2025_aaai_3.jpg', alt: '2025_aaai_3' },
             { src: process.env.PUBLIC_URL + '/gallery/2025_aaai_4.jpg', alt: '2025_aaai_4' },
             { src: process.env.PUBLIC_URL + '/gallery/2025_aaai_5.jpg', alt: '2025_aaai_5' },
-            { src: process.env.PUBLIC_URL + '/gallery/2025_aaai_7.jpg', alt: '2025_aaai_7' },
         ],
     },
     {
