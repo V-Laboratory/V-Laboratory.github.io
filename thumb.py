b90326b0308c4f0e48165c80628e87a4cfe4128c
Ubin108 <ybin0108@gmail.com>
2026-03-25 17:25:36 +0900

    edit paper info

--- thumb.py
+++ thumb.py
@@ -9,7 +9,7 @@ def pdf_to_thumbnail(pdf_path, output_path):
     except Exception as e:
         print(f"Error generating thumbnail: {e}")
 
-pdf_path = "/home/vlab/2603.21944v1.pdf"
+pdf_path = "/home/vlab/2603.21064v2.pdf"
 output_path = "./thumbnail.jpg"
 
 pdf_to_thumbnail(pdf_path, output_path)
